c6e5c29dd1611e56bd6c29f23879d80889dc737e
François Laignel <fengalin@free.fr>
2020-11-26 01:22:05 +0100

    more cleanups

--- fix-definition/src/fix.rs
+++ fix-definition/src/fix.rs
@@ -8,7 +8,7 @@ use crate::{Error, GetterVisitor};
 /// Fixes the file at the given path.
 ///
 /// If `output_path` is specified, the result will be written there,
-/// other the input files are overwitten.
+/// otherwise the input files are overwritten.
 pub(crate) fn fix(path: &Path, output_path: &Option<PathBuf>) -> Result<(), Error> {
     // Analyze Rust file
     let source_code = fs::read_to_string(path).map_err(Error::ReadFile)?;
@@ -33,41 +33,38 @@ pub(crate) fn fix(path: &Path, output_path: &Option<PathBuf>) -> Result<(), Erro
 
     // Write result
     let f = fs::File::create(output_path).map_err(Error::WriteFile)?;
-    {
-        let mut writer = std::io::BufWriter::new(f);
+    let mut writer = std::io::BufWriter::new(f);
 
-        let mut renamable_getters = getter_visitor.renamable_getters.iter();
-        let mut pending_rename = renamable_getters.next();
-        for (line_nb, line) in source_code.lines().enumerate() {
-            if let Some(pr) = pending_rename {
-                if line_nb == pr.line_nb {
-                    let origin = format!("fn {}(", pr.name);
-                    let target = format!("fn {}(", pr.new_name);
-
-                    // Add doc alias
-                    if pr.needs_doc_alias {
-                        writer
-                            .write_fmt(format_args!("#[doc(alias = \"{}\")] ", pr.name))
-                            .map_err(Error::WriteFile)?;
-                    }
-
-                    // Rename getter
+    let mut renamable_getters = getter_visitor.renamable_getters.iter();
+    let mut pending_rename = renamable_getters.next();
+    for (line_nb, line) in source_code.lines().enumerate() {
+        if let Some(pr) = pending_rename {
+            if line_nb == pr.line_nb {
+                if pr.needs_doc_alias {
                     writer
-                        .write(line.replacen(&origin, &target, 1).as_bytes())
+                        .write_fmt(format_args!("#[doc(alias = \"{}\")] ", pr.name))
                         .map_err(Error::WriteFile)?;
-
-                    pending_rename = renamable_getters.next();
-                } else {
-                    // Not the expected line => leave it unchanged
-                    writer.write(line.as_bytes()).map_err(Error::WriteFile)?;
                 }
+
+                // Rename getter
+                let origin = format!("fn {}(", pr.name);
+                let target = format!("fn {}(", pr.new_name);
+
+                writer
+                    .write(line.replacen(&origin, &target, 1).as_bytes())
+                    .map_err(Error::WriteFile)?;
+
+                pending_rename = renamable_getters.next();
             } else {
-                // No more getters to rename
+                // Not the expected line => leave it unchanged
                 writer.write(line.as_bytes()).map_err(Error::WriteFile)?;
             }
-
-            writer.write(&[b'\n']).map_err(Error::WriteFile)?;
+        } else {
+            // No more getters to rename
+            writer.write(line.as_bytes()).map_err(Error::WriteFile)?;
         }
+
+        writer.write(&[b'\n']).map_err(Error::WriteFile)?;
     }
 
     Ok(())
